938b6bc21f4273bb3b0390634b32abfb82e6d2fb
dceleste35 <danceleste974@gmail.com>
2024-06-09 14:42:08 +0200

    Transformation des variables en float

--- minitrice.py
+++ minitrice.py
@@ -15,5 +15,11 @@ def calculator(input_str):
 
     input_str = input_str.replace(' ', '')
 
+    for operator in '+-*/':
+        left, right = input_str.split(operator)
+
+    left_num = float(left)
+    right_num = float(right)
+
 if __name__ == "__main__":
     main()
